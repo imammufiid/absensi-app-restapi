7c7a74f1ccf7357585a1e66e6d0ced67a4114685
imufiid <imufiid98@gmail.com>
2021-02-26 13:18:54 +0700

    added field is_admin

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('nik')->unique();
             $table->string('email')->unique();
+            $table->tinyInteger('is_admin');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
