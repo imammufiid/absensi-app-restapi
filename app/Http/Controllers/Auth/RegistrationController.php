6cea1c3680ff03dba8a1dd7c3a6eeea9568e5e87
imufiid <imufiid98@gmail.com>
2021-02-26 14:06:46 +0700

    add validation register

--- app/Http/Controllers/Auth/RegistrationController.php
+++ app/Http/Controllers/Auth/RegistrationController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 class RegistrationController extends Controller
@@ -16,8 +17,27 @@ class RegistrationController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(RegisterRequest $request)
+    public function __invoke(Request $request)
     {
+
+        // validation 
+        $validation = Validator::make($request->all(), [
+            'name' => 'required|min:3|max:25',
+            'nik' => 'required|unique:users,nik',
+            'email' => 'email|required|unique:users,email',
+            'password' => 'required|min:8'
+        ]);
+
+        if ($validation->fails()) {
+            return response()->json([
+                'meta' => object_meta(
+                    Response::HTTP_BAD_REQUEST, 
+                    "failed", 
+                    "Failed"),
+                'data' => $validation->errors()
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
         $user = User::create([
             'name' => request('name'),
             'email' => request('email'),
