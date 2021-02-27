a6b0fe6e661f79d0b389d97cf9a3d7ffada738a1
imufiid <imufiid98@gmail.com>
2021-02-27 20:47:09 +0700

    create api endpoint task

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,13 @@ Route::namespace('Auth')->prefix('auth')->group(function () {
     Route::post('login', 'LoginController');
     Route::post('logout', 'LogoutController');
 });
+
+Route::group([
+    "namespace" => 'Task',
+    "prefix"    => 'task'
+], function() {
+    Route::get('/', 'TaskController@index');
+    Route::get('/show', 'TaskController@show');
+    Route::post('/mark', 'TaskController@markComplete');
+    Route::post('/store', 'TaskController@store');
+});
