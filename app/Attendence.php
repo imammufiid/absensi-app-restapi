38237b91b86e8618c5547112e5d012a8d4ff59f9
imufiid <imufiid98@gmail.com>
2021-02-27 10:55:26 +0700

    add fillable model

--- app/Attendence.php
+++ app/Attendence.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attendence extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'date', 'time_comes', 'time_gohome'
+    ];
 }
