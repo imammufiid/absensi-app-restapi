0a9cab1eb945d47aa60a675a0b65a4d84b2dcbbc
imufiid <imufiid98@gmail.com>
2021-04-05 09:15:33 +0700

    add : checking folder if exist and create folder

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Config;
+use Illuminate\Support\Facades\File;
 
 if (!function_exists('object_meta')) {
    function object_meta($code = 200, $status = "success", $message = "")
@@ -25,5 +26,26 @@ if (!function_exists('list_of_name_configuration')) {
    function list_of_name_configuration() {
       $data = Config::select('name')->get();
       return $data;
+   }
+}
+
+if (!function_exists('create_folder_public')) {
+   function create_folder_public($nameOfFolder = "") {
+      $folderPath = public_path($nameOfFolder);
+      if (!File::isDirectory($folderPath)) {
+         File::makeDirectory($folderPath, 0777, true, true);
+         return $folderPath;
+      }
+      return $folderPath;
+   }
+}
+
+if (!function_exists('check_folder_public_if_exist')) {
+   function check_folder_public_if_not_exist($nameOfFolder = "") {
+      $folderPath = public_path($nameOfFolder);
+      if (!File::isDirectory($folderPath)) {
+         return true;
+      }
+      return false;
    }
 }
