af6c55f9cb0a394056f55bc5c9e5b80f3679144c
imufiid <imufiid98@gmail.com>
2021-04-19 10:14:56 +0700

    changes config seeder

--- database/seeds/ConfigSeeder.php
+++ database/seeds/ConfigSeeder.php
@@ -15,13 +15,16 @@ class ConfigSeeder extends Seeder
         $configLocationOffice = new Config();
 
         $dataOfficeLocation = [
-            "name"  => "Office",
-            "address"   => "Blitar City",
-            "latitude"  => "0.0000000",
-            "longitude"  => "000.0000000",
+            "name"              => "Office",
+            "address"           => "Blitar City",
+            "latitude"          => "0.0000000",
+            "longitude"         => "000.0000000",
+            "time"              => "07:30:00",
+            "attendance_point"  => 2,
+            "task_point"        => 2
         ];
 
-        $configLocationOffice->name = "office location";
+        $configLocationOffice->name = "office";
         $configLocationOffice->configuration = json_encode($dataOfficeLocation);
         $configLocationOffice->status = 1;
         $configLocationOffice->save();
