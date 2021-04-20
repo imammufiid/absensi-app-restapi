e23791e1e5407dc2a6f3e1f15cadaa0ebb1efeaf
imufiid <imufiid98@gmail.com>
2021-04-20 10:20:53 +0700

    add const criteria

--- app/MyCons.php
+++ app/MyCons.php
@@ -9,4 +9,11 @@ class MyCons
     const Cukup = 3;
     const Tinggi = 4;
     const SangatTinggi = 5;
+
+    const Kedisiplinan = 1;
+    const kejujuran = 2;
+    const Komunikasi = 3;
+    const Kerjasama = 4;
+    const TanggungJawab = 5;
+
 }
