77c80ef07b4404d58b822c6ff86cb399f2e2cb54
imufiid <imufiid98@gmail.com>
2021-05-04 09:05:58 +0700

    create migration and model location detail for lbs

---
+++ app/LocationDetail.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AttendanceDetail extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'attendance_id', 'latitude', 'longitude', 'lbs'
+    ];
+}
---
+++ database/migrations/2021_05_04_090228_create_location_details_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocationDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('location_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("attendance_id");
+            $table->string("latitude");
+            $table->string("longitude");
+            $table->string("lbs");
+            $table->timestamps();
+
+            $table->foreign("attendance_id")
+                ->references("id")
+                ->on("attendences")
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('location_details');
+    }
+}
