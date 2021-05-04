f85339c6f442017f72aa3adaf42e73f1643a6357
imufiid <imufiid98@gmail.com>
2021-05-04 10:16:01 +0700

    rename name

--- app/LocationDetail.php
+++ app/LocationDetail.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class AttendanceDetail extends Model
+class LocationDetail extends Model
 {
     /**
      * The attributes that are mass assignable.
